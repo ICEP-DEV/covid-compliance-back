41e4d81c0abce96bf1b7f2207ddc4b939524c9e8
Mrgiggs1 <rudolphpuane@gmial.com>
2021-08-13 15:29:50 +0200

    commit back end

---
+++ routes/user.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const Router = express.Router();
+const connection = require('../connection');
+//const tut_database = require('../tut_database_con');
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+
+Router.get('/staff', (req, res) => {
+    connection.query('SELECT * FROM user where role = "staff"', (err, rows, fields) => {
+        if(!err){
+            res.send(rows)
+        }else{
+            console.log(err)
+        }
+    })
+});
+
+Router.get('/student', (req, res) => {
+    connection.query('SELECT * FROM user where role = "student"', (err, rows, fields) => {
+        if(!err){
+            res.send(rows)
+        }else{
+            console.log(err)
+        }
+    })
+});
+
+module.exports = Router;

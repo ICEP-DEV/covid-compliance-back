62a8445f600cd1cd5ceca91c0a443de3b4bc266c
mchauke250 <mchauke250@gmail.com>
2021-09-08 23:14:35 +0200

    statUpdate

--- routes/statistics.js
+++ routes/statistics.js
@@ -79,6 +79,133 @@ Router.get('/campus/symptoms', (req, res) => {
     })
 });
 
+//Weekly
+Router.get('/campus/weekly', (req, res) => {
+    connection.query('SELECT count(*) as "totnum" FROM screen where camp_id = "soshn" and screen_date = (SELECT DATE(SYSDATE()))', (err, rows, fields) => {
+        if(!err){
+            res.send(rows)
+        }else{
+            console.log(err)
+        }
+    })
+});
+
+Router.get('/campus/student/weekStud', (req, res) => {
+    connection.query('SELECT count(*) as "studNum" FROM screen,user where camp_id = "soshn" and screen_date = (SELECT DATE(SYSDATE())) and role = "student"', (err, rows, fields) => {
+        if(!err){
+            res.send(rows)
+        }else{
+            console.log(err)
+        }
+    })
+});
+
+Router.get('/campus/staff/weekStaff', (req, res) => {
+    connection.query('SELECT count(*) as "staffNum" FROM screen,user where camp_id = "soshn" and screen_date = (SELECT DATE(SYSDATE())) and role = "staff"', (err, rows, fields) => {
+        if(!err){
+            res.send(rows)
+        }else{
+            console.log(err)
+        }
+    })
+});
+
+Router.get('/campus/constractor/weekCons', (req, res) => {
+    connection.query('SELECT count(*) as "constNum" FROM screen,user where camp_id = "soshn" and screen_date = (SELECT DATE(SYSDATE())) and role = "constractor"', (err, rows, fields) => {
+        if(!err){
+            res.send(rows)
+        }else{
+            console.log(err)
+        }
+    })
+});
+
+Router.get('/campus/visitor/weekVis', (req, res) => {
+    connection.query('SELECT count(*) as "visNum" FROM screen,user where camp_id = "soshn" and screen_date = (SELECT DATE(SYSDATE())) and role = "visitor"', (err, rows, fields) => {
+        if(!err){
+            res.send(rows)
+        }else{
+            console.log(err)
+        }
+    })
+});
+
+Router.get('/campus/symptoms/weekSymp', (req, res) => {
+    connection.query('SELECT count(*) as "sympNum" FROM screen,user where camp_id = "soshn" and screen_date = (SELECT DATE(SYSDATE())) and temp > 36', (err, rows, fields) => {
+        if(!err){
+            res.send(rows)
+        }else{
+            console.log(err)
+        }
+    })
+});
+
+//end weekly
+
+//beginning of monthly
+// we only need to change the date by month
+Router.get('/campus/monthly', (req, res) => {
+    connection.query('SELECT count(*) as "totnum" FROM screen where camp_id = "soshn" and screen_date = (SELECT DATE(SYSDATE()))', (err, rows, fields) => {
+        if(!err){
+            res.send(rows)
+        }else{
+            console.log(err)
+        }
+    })
+});
+
+Router.get('/campus/student/monthStud', (req, res) => {
+    connection.query('SELECT count(*) as "studNum" FROM screen,user where camp_id = "soshn" and screen_date = (SELECT DATE(SYSDATE())) and role = "student"', (err, rows, fields) => {
+        if(!err){
+            res.send(rows)
+        }else{
+            console.log(err)
+        }
+    })
+});
+
+Router.get('/campus/staff/monthStaff', (req, res) => {
+    connection.query('SELECT count(*) as "staffNum" FROM screen,user where camp_id = "soshn" and screen_date = (SELECT DATE(SYSDATE())) and role = "staff"', (err, rows, fields) => {
+        if(!err){
+            res.send(rows)
+        }else{
+            console.log(err)
+        }
+    })
+});
+
+Router.get('/campus/constractor/monthCons', (req, res) => {
+    connection.query('SELECT count(*) as "constNum" FROM screen,user where camp_id = "soshn" and screen_date = (SELECT DATE(SYSDATE())) and role = "constractor"', (err, rows, fields) => {
+        if(!err){
+            res.send(rows)
+        }else{
+            console.log(err)
+        }
+    })
+});
+
+Router.get('/campus/visitor/monthVis', (req, res) => {
+    connection.query('SELECT count(*) as "visNum" FROM screen,user where camp_id = "soshn" and screen_date = (SELECT DATE(SYSDATE())) and role = "visitor"', (err, rows, fields) => {
+        if(!err){
+            res.send(rows)
+        }else{
+            console.log(err)
+        }
+    })
+});
+
+Router.get('/campus/symptoms/monthSymp', (req, res) => {
+    connection.query('SELECT count(*) as "sympNum" FROM screen,user where camp_id = "soshn" and screen_date = (SELECT DATE(SYSDATE())) and temp > 36', (err, rows, fields) => {
+        if(!err){
+            res.send(rows)
+        }else{
+            console.log(err)
+        }
+    })
+});
+
+//end of montly
+
 Router.get('/hotspot/:campu', (req, res) => {
     connection.query('SELECT count(*) as "total_hotspot" FROM screen where camp_id = "'+ req.params.campu +'" and temp > 36.9' , (err, rows, fields) => {
         if(!err){

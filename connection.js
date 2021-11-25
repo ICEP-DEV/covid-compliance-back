a57a91221419226735f4a949e57af07f5ff511e3
SimNyandeni <sisehahabane@gmail.com>
2021-11-26 01:28:49 +0200

    added nodemon

--- connection.js
+++ connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'covid_compliance'
-  })
+    host: 'us-cdbr-east-04.cleardb.com',
+    user: 'bce450d24b66fe',
+    password: '9c1fa31b',
+    database: 'heroku_88bb8ef8f2c3882'
+  }) //mysql://bce450d24b66fe:9c1fa31b@us-cdbr-east-04.cleardb.com/heroku_88bb8ef8f2c3882?reconnect=true
   
   connection.connect()
   
